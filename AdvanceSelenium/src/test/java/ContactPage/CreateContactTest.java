27798e358438d3083decbf522dee871781672083
HP <HP@DESKTOP-AOMMAF5>
2025-07-11 08:13:11 +0530

    Merge branch 'master' of https://github.com/royabhishek1303/AdvanceSelenium_new1.git

--- AdvanceSelenium/src/test/java/ContactPage/CreateContactTest.java
+++ AdvanceSelenium/src/test/java/ContactPage/CreateContactTest.java
@@ -24,12 +24,12 @@ import Pom_Repo.ProductPage;
 public class CreateContactTest extends Base_Class {
 	
 	
-	//push final code
+	// push the code
 	@Test(groups = "regression")
 	public void createContactTest() throws Throwable {
 		
 
-		// hi Abhishek--->eng2Push
+		
 		HomePage home = new HomePage(driver);
 		home.clickTOContactLink();
 		
